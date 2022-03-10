c0d6b843c8f811a9aabaffec8030e32e84fd6f1d
LeandroDelinger <leandro.delinger@gmail.com>
2022-03-10 20:00:36 -0300

    Integração com banco de dados, regras de negócios e configuração do stripe completa

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth";
+import NextAuth, { User, Account, Profile } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
+
+import { query as q } from "faunadb";
 
 export default NextAuth({
   providers: [
@@ -13,4 +17,29 @@ export default NextAuth({
       },
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
+  },
 });
